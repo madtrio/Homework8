2ca9c769297151604db31a8c08ed3f06457d4faa
madtro <anna.kozitzyna@yandex.ru>
2023-01-22 19:20:46 +0300

    task 3

--- src/Main.java
+++ src/Main.java
@@ -19,10 +19,10 @@ public class Main {
         System.out.println("Task 1");
         int[] arr = generateRandomArray();
         int sum1 = 0;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < 30; i++) {
             sum1 = sum1 + arr[i];
         }
-        System.out.println("Сумма трат за месяц составила "+ sum1 + " рублей");
+        System.out.println("Сумма трат за месяц составила " + sum1 + " рублей");
     }
 
     public static void task2() {
@@ -30,7 +30,6 @@ public class Main {
         int[] arr = generateRandomArray();
         int max = 0;
         int min = arr[4];
-        System.out.println(min);
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
@@ -39,14 +38,19 @@ public class Main {
                 min = arr[i];
             }
         }
-        System.out.println("Максимальная суточная трата за месяц составила "+ max + " рублей");
-        System.out.println("Минимальная суточная трата за месяц составила "+ min + " рублей");
+        System.out.println("Максимальная суточная трата за месяц составила " + max + " рублей");
+        System.out.println("Минимальная суточная трата за месяц составила " + min + " рублей");
     }
 
     public static void task3() {
         System.out.println("Task 3");
         int[] arr = generateRandomArray();
-
+        int sum3 = 0;
+        for (int i = 0; i < 30; i++) {
+            sum3 = sum3 + arr[i];
+        }
+        double mean3 = sum3/30f;
+        System.out.println("Средняя сумма трат за месяц составила " + mean3 + " рублей");
     }
 
     public static void task4() {
